05605154d088ee9dda935321b5d88c57ba8a2992
Wille Lee <1992season@gmail.com>
2024-07-12 20:33:17 +0900

    [EDIT] ci

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,9 +1,3 @@
-import { signup } from "@libs/actions";
-
 export default function AuthSignupPage() {
-  return (
-    <form action={signup}>
-      <button type="submit">signup</button>
-    </form>
-  );
+  return <>sign up</>;
 }
